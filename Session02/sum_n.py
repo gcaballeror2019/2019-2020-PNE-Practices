53f0d7708c47d58232891a7608aedd8a4753c1ac
gregocr <elmodirect17@gmail.com>
2020-02-04 15:58:16 +0100

    This program sums the numbers from 1 to "n"

---
+++ Session02/sum_n.py
@@ -0,0 +1,13 @@
+# 1 + 2 + 3 + ... + 20
+# 1 + ... + 100
+
+
+def sum_n(n):
+    res_n = 0
+    for i in range(0,n):
+        res_n += i + 1
+    return res_n
+
+
+print("Sum of the numbers 1-20 is:", sum_n(20))
+print("Sum of the numbers 1-100 is:", sum_n(100))
