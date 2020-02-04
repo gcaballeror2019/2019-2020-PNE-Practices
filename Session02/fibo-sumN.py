65332634bc4a337810951fd13dd129b5e423fd5b
gregocr <elmodirect17@gmail.com>
2020-02-04 16:24:38 +0100

    This program calculates the 5th, 10th and 15th terms of the Fibonacci sequence

--- Session02/fibo-sumN.py
+++ Session02/fibo-sumN.py
@@ -3,3 +3,16 @@
 
 
 def fibosum(n):
+    value_0 = 0
+    value_1 = 1
+    result = 0
+    for i in range(0, n):
+        n_val = value_1 + value_0
+        value_0 = value_1
+        value_1 = n_val
+        result += value_0
+    return result
+
+
+print("The sum of the first 5 terms of the Fibonacci series is:",fibosum(5))
+print("The sum of the first 10 terms of the Fibonacci series is:",fibosum(10))
