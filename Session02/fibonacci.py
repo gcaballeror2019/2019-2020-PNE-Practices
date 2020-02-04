b83c6a2d0923523b2e87ba68995df312d4e79b03
gregocr <elmodirect17@gmail.com>
2020-02-04 16:11:01 +0100

    This program calculates the first 11th numbers of the Fibonacci sequence

---
+++ Session02/fibonacci.py
@@ -0,0 +1,11 @@
+#Session 2, Ex 1
+#Print the first eleven numbers of the Fibonacci series
+
+value_0 = 0
+value_1 = 1
+
+for i in range(0, 11):
+    print(value_0, end=" ")
+    n_val = value_1 + value_0
+    value_0 = value_1
+    value_1 = n_val
