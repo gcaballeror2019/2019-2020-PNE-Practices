742fd57da55f418b7ad27713991fa25d66361eee
gregocr <elmodirect17@gmail.com>
2020-02-05 17:16:54 +0100

    There is an error in the result 2 due to a division by zero

---
+++ Session03/FindTheBug.py
@@ -0,0 +1,22 @@
+# --- Find the error!
+
+
+def g(a, b):
+    return a - b
+
+
+def f(a, b, c, d):
+    t0 = a + b - g(a, 0)
+    t1 = g(c, d)
+    t3 = 2 * (t0 / t1)
+    return t0 + 2*t1 + t3*t3
+
+
+# -- Main program
+print("Result 1: ", f(5, 2, 5, 0))
+
+# -- There is an error here regarding the value t3 in the function 'f', it tries to divide by zero
+print("Result 2: ", f(0, 2, 3, 3))
+
+print("Result 3: ", f(1, 3, 2, 3))
+print("Result 4: ", f(1, 9, 22.0, 3))
