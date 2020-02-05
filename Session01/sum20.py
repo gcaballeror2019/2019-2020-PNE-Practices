a5600c4659006a666999d03a1733bac806d3be4d
gregocr <elmodirect17@gmail.com>
2020-02-05 17:39:10 +0100

    This program sums the numbers from 1 to 20

--- Session01/sum20.py
+++ Session01/sum20.py
@@ -1,10 +1,12 @@
-#Session 1, Ex 3
-#Calc the sum of the first 20 int. numb.
+# -- Session 1, Ex 3
+# -- Calc the sum of the first 20 int. numb.
 
-#Store the result
+# -- Store the result
+
+
 result = 0
 
-for i in range (0,20):
+for i in range(0, 20):
     result += i+1
 
 print("The sum is:", result)
