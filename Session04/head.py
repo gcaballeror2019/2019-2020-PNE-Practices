ea9b23f4f26121fef5418e039532960bbb8a7275
gregocr <elmodirect17@gmail.com>
2020-02-11 16:25:38 +0100

    Session04 Ex5 + minor grammar fixes

--- Session04/head.py
+++ Session04/head.py
@@ -6,7 +6,7 @@ FILENAME = "RNU6_269P.txt"
 # -- Constant with the new of the file to open
 file_contents = Path(FILENAME).read_text()
 
-# -- Split the dile into two parts, the sequence and the header
+# -- Split the file into two parts, the sequence and the header
 file_header = file_contents.split('\n', 1)
 
 # -- Print the contents on the console (Only the header)
--- Session04/sequence.py
+++ Session04/sequence.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # -- Constant with the new of the file to open
-FILENAME = "RNU6_269P.txt"
+FILENAME = "ADA.txt"
 
 # -- Constant with the new of the file to open
 file_contents = Path(FILENAME).read_text()
@@ -9,5 +9,8 @@ file_contents = Path(FILENAME).read_text()
 # -- Split the file into two parts, the sequence and the header
 file_body = file_contents.split('\n', 1)
 
-# -- Print the contents on the console (Only the body)
-print(file_body[1])
+# -- We replace the line-jump with an empty character
+file_body = file_body[1].replace('\n', '')
+
+# -- Print the number of bases on the console
+print("The number of bases is:", len(file_body))
