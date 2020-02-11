6fb630e27b96dddad0b74fb4ee5326954fa97534
gregocr <elmodirect17@gmail.com>
2020-02-11 16:09:59 +0100

    Session04 Ex3

--- Session04/head.py
+++ Session04/head.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
+# -- Constant with the new of the file to open
 FILENAME = "RNU6_269P.txt"
 
+# -- Constant with the new of the file to open
 file_contents = Path(FILENAME).read_text()
 
+# -- Split the dile into two parts, the sequence and the header
 file_header = file_contents.split('\n', 1)
 
+# -- Print the contents on the console
 print(file_header[0])
