867e69f2bae931cd51aa45a85bee6ee525e71c4a
gregocr <elmodirect17@gmail.com>
2020-02-12 17:26:51 +0100

    Seq0 Update

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+
+
 def seq_ping():
     print('OK!')
+
+
+def seq_read_fasta(filename):
+    file_contents = Path(filename).read_text()
+    file_body = file_contents.split('\n', 1)
+    file_body = file_body[1].replace('\n', '')
+    char = 0
+    while char <= 19:
+        print(file_body[char])
