bc9d6ff8cf4f3526835d5dbe8f499d4e0b397b88
gregocr <elmodirect17@gmail.com>
2020-02-19 18:45:50 +0100

    Session 6 Ex1 & 2 & 3

--- Session06/Ex1.py
+++ Session06/Ex1.py
@@ -1,12 +1,20 @@
 class Seq:
     """A class for representing sequences"""
     def __init__(self, strbases):
-        self.strbases = strbases
-        print("New sequence created!")
+        codon = ['A', 'T', 'G', 'C']
+        valid = True
+        for i in strbases:
+            if i not in codon:
+                valid = False
+        if valid:
+            print("New sequence created!")
+            self.strbases = strbases
+        else:
+            print("INCORRECT Sequence detected")
+            self.strbases = 'ERROR'
 
     def __str__(self):
         """Method called when the object is being printed"""
-
         # -- We just return the string with the sequence
         return self.strbases
 
@@ -33,8 +41,34 @@ class Gene(Seq):
         return self.name + "-" + self.strbases
 
 
+def print_seqs(seq_list):
+    num_seq = 0
+    for i in seq_list:
+        print(f'Sequence {num_seq}:(Length: {i.len()}) {i}')
+        num_seq += 1
+
+
+def generate_seqs(pattern, number):
+    new_seq = []
+    for _ in range(number):
+        pattern += pattern
+    return new_seq
+
+
 # --- Main program
 s1 = Seq("ACCTGC")
 s2 = Seq("Hello? Am I a valid sequence?")
+seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
 print(f"Sequence 1: {s1}")
 print(f"Sequence 2: {s2}")
+print_seqs(seq_list)
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+print(seq_list1)
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
