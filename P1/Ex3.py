55dd73e375c5360193a6f57f6f4526e974c89975
gcaballeror2019 <NivMizzet74>
2020-02-26 20:48:23 +0100

    Practice 1 Ex3

---
+++ P1/Ex3.py
@@ -0,0 +1,11 @@
+from Seq1 import Seq
+
+
+seq_list1 = Seq()
+seq_list2 = Seq('ACTGA')
+seq_list3 = Seq('ACDSFG')
+
+
+print(f'Sequence 1: {seq_list1}')
+print(f'Sequence 2: {seq_list2}')
+print(f'Sequence 3: {seq_list3}')
