c8ec54250a7f45c5024746cbb0ad80b9f6ac7b42
gcaballeror2019 <NivMizzet74>
2020-02-26 20:34:18 +0100

    Practice 1 Ex1

---
+++ P1/Ex1.py
@@ -0,0 +1,5 @@
+from Seq1 import Seq
+
+
+seq_list = Seq('ACTGA')
+print(f'Sequence 1:(Length: {seq_list.len()}) {seq_list}')
---
+++ P1/Seq1.py
@@ -0,0 +1,60 @@
+import termcolor
+
+
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        codon = ['A', 'T', 'G', 'C']
+        valid = True
+        for i in strbases:
+            if i not in codon:
+                valid = False
+        if valid:
+            print("New sequence created!")
+            self.strbases = strbases
+        else:
+            print("INCORRECT Sequence detected")
+            self.strbases = 'ERROR'
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+        # -- We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
+
+
+class Gene(Seq):
+    """This class is derived from the Seq Class
+       All the objects of class Gene will inheritate
+       the methods from the Seq class
+    """
+    def __init__(self, strbases, name=""):
+
+        # -- Call first the Seq initilizer and then the
+        # -- Gene init method
+        super().__init__(strbases)
+        self.name = name
+        print("New gene created")
+
+    def __str__(self):
+        """Print the Gene name along with the sequence"""
+        return self.name + "-" + self.strbases
+
+
+def print_seqs(seq_list, colour):
+    num_seq = 0
+    for i in seq_list:
+        ''' We improve the function by adding the 
+        termcolor function to add colour to the sequences printed'''
+        termcolor.cprint(f'Sequence {num_seq}:(Length: {i.len()}) {i}', colour)
+        num_seq += 1
+
+
+def generate_seqs(pattern, number):
+    new_seq = []
+    for i in range(number):
+        new_seq.append(Seq(pattern*(i+1)))
+    return new_seq
