b3d28233a16917e6d17af1fc66ad1871a15a1026
gregocr <elmodirect17@gmail.com>
2020-03-04 18:19:42 +0100

    Practice 3 Ex 1, 2, & 3 (No comments yet)

---
+++ P3/Seq-server.py
@@ -0,0 +1,119 @@
+import socket
+from termcolor import colored
+from Seq1 import Seq
+
+
+def ping():
+    response = f'OK!\n'
+    print(response)
+    return cs.send(str.encode(response))
+
+
+def spl_in(in_msg):
+    return in_msg.split(' ')[1]
+
+
+def percentage(partial, tot):
+    return round((partial*100)/tot, 1)
+
+
+def get_seq(in_msg):
+    index = int(spl_in(in_msg))
+    response = dna_list[index]
+    print(response)
+    return cs.send(str.encode(response))
+
+
+def info_seq(in_msg):
+    get = Seq(spl_in(in_msg))
+    f_msg = f'Sequence:{get}\n'
+    len_seq = get.len()
+    f_msg += f'Total length: {len_seq}\n'
+    count_seq = get.count()
+    for k, v in count_seq.items():
+        perc = percentage(v, len_seq)
+        codon = f'{k}: {v} ({perc}%)\n'
+        f_msg += codon
+    response = f_msg
+    print(response)
+    return cs.send(str.encode(response))
+
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1"
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("SEQ server configured!")
+nc = 0
+client_ip_list = []
+dna_list = ['ACTG', 'ACTGA', 'ACTGAC', 'ACTGACT', 'ACTGACTG']
+while True:
+    if nc < 5:
+        # -- Waits for a client to connect
+        print("Waiting for Clients... ")
+
+        try:
+            (cs, client_ip_port) = ls.accept()
+
+        # -- Server stopped manually
+        except KeyboardInterrupt:
+            print("Server stopped by the user")
+
+            # -- Close the listenning socket
+            ls.close()
+
+            # -- Exit!
+            exit()
+
+        # -- Execute this part if there are no errors
+        else:
+
+            nc += 1
+            client_ip_list.append(f'Client{nc}:{client_ip_port}')
+            print("CONNECTION: {} From the IP: {}".format(nc, client_ip_port))
+
+            # -- Read the message from the client
+            # -- The received message is in raw bytes
+            msg_raw = cs.recv(2048)
+
+            # -- We decode it for converting it
+            # -- into a human-redeable string
+            msg = msg_raw.decode()
+
+            if msg == 'PING':
+                print(colored('PING command!', 'yellow'))
+                ping()
+                cs.close()
+            elif 'GET' in msg:
+                print(colored('GET command!', 'green'))
+                get_seq(msg)
+                cs.close()
+            elif 'INFO' in msg:
+                print(colored('INFO command!', 'blue'))
+                info_seq(msg)
+                cs.close()
+
+    else:
+        print(f'The following clients have sended a message to the server:')
+        for i in client_ip_list:
+            print(i)
+        ls.close()
+        exit()
---
+++ P3/test-client.py
@@ -0,0 +1,12 @@
+from Client0 import Client
+
+
+# -- Parameters of the server to talk to
+IP = "127.0.0.1"
+PORT = 8080
+
+# -- Create a client object
+c = Client(IP, PORT)
+print("Sending a message to the server...")
+response = c.talk("INFO ACTGTGTGTGTGTGTACTGATG")
+print(response)
