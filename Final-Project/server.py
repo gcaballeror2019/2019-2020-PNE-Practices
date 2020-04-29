b4d11b39e5d113c8607941237fc56cd9752286a2
gcaballeror2019 <60136538+gcaballeror2019@users.noreply.github.com>
2020-04-29 19:36:23 +0200

    FP Ini

---
+++ Final-Project/server.py
@@ -0,0 +1,189 @@
+from Seq1 import Seq
+from pathlib import Path
+import http.server
+import termcolor
+import socketserver
+import json
+import http.client
+
+
+# Server's Port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# -- Server used for the project
+server = 'rest.esembl.org'
+params = '?content-type=application/json'
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inherits all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # -- Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # -- Analyze the request line
+        req_line = self.requestline.split(' ')
+
+        # -- Get the path. It always start with the / symbol
+        path = req_line[1]
+
+        # --  Check all the arguments
+        arguments = path.split('?')
+
+        # -- The verb is located as the first argument
+        verb = arguments[0]
+
+        contents = Path('Error.html').read_text()
+        status = 404
+
+        # -- Gives a response depending of the existence of de
+        if verb == '/':
+            # Open index file (HTML)
+            contents = Path('index').read_text()
+
+        elif verb == '/listSpecies':
+            # Verification page to show that the server is on-line
+            contents = '''
+                        <!DOCTYPE html>
+                        <html lang='en'>
+                        <head>
+                            <meta charset='utf-8'>
+                            <title>List of species</title>
+                        </head>
+                        <body>
+                        <p> The total number of species in ensembl is: 267 </p>'''
+
+            status = 200
+        elif verb == '/karyotype':
+            # -- HTML
+            contents = f"""
+                        <!DOCTYPE html>
+                        <html lang = "en">
+                        <head>
+                        <meta charset = "utf-8" >
+                          <title> GET </title >
+                        </head >
+                        <body>
+                        <h2>Karyotype</h2>
+                        <p>The names of the chromosomes are:</p>"""
+
+
+            status = 200
+
+        elif verb == '/chromosomeLength':
+            # -- Get the argument to the right of the ? symbol
+            pair = arguments[1]
+            # -- Get all the pairs name = value
+            pairs = pair.split('&')
+            # -- Get the two elements: name and value
+            s_name, spce = pairs[0].split('=')
+            ch_index, chr = pairs[1].split('=')
+            # -- Generate the html code
+            contents = f"""
+                                   <!DOCTYPE html>
+                                   <html lang = "en">
+                                   <head>
+                                   <meta charset = "utf-8" >
+                                     <title> GENE </title >
+                                   </head >
+                                   <body>
+                                   <h2> Gene: {gene}</h2>
+                                   <textarea readonly rows="20" cols="80"> {gene_str} </textarea>
+                                   <br>
+                                   <br>
+                                   <a href="/">Main page</a>
+                                   </body>
+                                   </html>
+                                   """
+            status = 200
+        elif verb == '/geneSeq':
+            # -- Get the argument to the right of the ? symbol
+            pair = arguments[1]
+            # -- Get all the pairs name = value
+            pairs = pair.split('&')
+            # -- Get the two elements: name and value, and the operation name w/ the chosen operation
+            name, seq = pairs[0].split('=')
+            o_name, operation = pairs[1].split('=')
+            seq = Seq(seq)
+            if operation == 'Rev':
+                result = seq.reverse()
+            elif operation == 'Comp':
+                result = seq.complement()
+            else:
+                # We calculate the length, amount of bases and the percentage they occupy in the sequence
+                g_len = seq.len()
+                counter = seq.count()
+                per_a = 100 * int(counter['A']) / g_len
+                per_c = 100 * int(counter['C']) / g_len
+                per_t = 100 * int(counter['T']) / g_len
+                per_g = 100 * int(counter['G']) / g_len
+                result = f"""
+                    <p>Total length: {g_len}</p>
+                    <p>A: {counter['A']} ({per_a}%)</p>
+                    <p>C: {counter['C']} ({per_c}%)</p>
+                    <p>G: {counter['T']} ({per_t}%)</p>
+                    <p>T: {counter['G']} ({per_g}%)</p>"""
+            contents = f"""
+                <!DOCTYPE html>
+                <html lang = "en">
+                <head>
+                <meta charset = "utf-8" >
+                    <title> Operations </title >
+                </head >
+                <body>
+                <h2> Seq:</h2>
+                <p>{seq}</p>
+                <h2> Operation: </h2>
+                <p>{operation}</p>
+                <h2> Result: </h2>
+                <p>{result}</p>
+                <br>
+                <br>
+                <a href="/">Main page</a>
+                </body>
+                </html>
+                """
+            status = 200
+        # -- Generating the response message
+        self.send_response(status)
+
+        # -- Define the content-type header:
+        content_type = 'text/html'
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # -- The header is finished
+        self.end_headers()
+
+        # -- Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(('', PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('')
+        print('Stopped by the user')
+        httpd.server_close()
